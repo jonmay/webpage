94110aff7f879c14049149c5cbe4d55b4b7c610d
Jon May <jonmay@isi.edu>
2022-07-06 13:52:25 -0700

    initial cutelabname files

---
+++ cutelabname/fun.js
@@ -0,0 +1,62 @@
+var fun_data = [
+	{
+		'flickr_link': 'https://www.flickr.com/photos/145494557@N05/albums/72157709869134676/player/',
+		'img_link': '',
+		'text': 'Lab Pictures',
+		'description': ''
+	},
+	{
+		'flickr_link': 'https://www.flickr.com/photos/183261364@N02/albums/72157712155173278/player/',
+		'img_link': '',
+		'text': 'Outing 1',
+		'description': ''
+	},
+	{
+		'flickr_link': 'https://www.flickr.com/photos/183261364@N02/albums/72157710188225316/player/',
+		'img_link': '',
+		'text': 'Outing 2',
+		'description': ''
+	},
+	{
+		'flickr_link': '',
+		'img_link': 'https://pbs.twimg.com/media/FNhMPe5VkAklZ3d?format=jpg&name=4096x4096',
+		'text': 'Van Gogh Exhibit Visit 2022',
+		'description': ''
+	},
+	{
+		'flickr_link': '',
+		'img_link': 'https://www.isi.edu/~jonmay/imgs/2019_summer_hike.jpg',
+		'text': 'Summer Hike 2019',
+		'description': ''
+	},
+	{
+		'flickr_link': '',
+		'img_link': 'https://www.isi.edu/~jonmay/imgs/2019_summer_lunch.jpg',
+		'text': 'Summer Lunch 2019',
+		'description': ''
+	},
+	{
+		'flickr_link': '',
+		'img_link': 'https://www.isi.edu/~jonmay/imgs/2019_winter_hike_1a.jpg',
+		'text': 'Winter Hike 2019',
+		'description': ''
+	},
+	{
+		'flickr_link': '',
+		'img_link': 'https://www.isi.edu/~jonmay/imgs/2019_winter_hike_2.jpg',
+		'text': 'More Winter Hike 2019',
+		'description': ''
+	},
+	{
+		'flickr_link': '',
+		'img_link': 'https://www.isi.edu/~jonmay/imgs/2018_group_1.jpg',
+		'text': 'Summer Group Pic 2018',
+		'description': ''
+	},
+	{
+		'flickr_link': '',
+		'img_link': 'https://www.isi.edu/~jonmay/imgs/2018_group_2.jpg',
+		'text': 'Another Summer Group Pic 2018',
+		'description': ''
+	},
+]
